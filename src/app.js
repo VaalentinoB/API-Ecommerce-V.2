bcdc5c86f6b35522469312dfc1f6327744be5383
VaalentinoB <valentinoburioni@hotmail.com>
2024-06-24 19:00:12 -0300

    hand

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cartsRouter from './routes/carts.routes.js';
+import { engine } from 'express-handlebars';
+import { Server } from 'socket.io';
 import productsRouter from './routes/products.routes.js';
 import displayRoutes from 'express-routemap';
 
@@ -8,17 +10,8 @@ const puerto = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
-
 app.use('/api/products', productsRouter);
 app.use('/api/cart', cartsRouter);
-
-
-app.listen(puerto, () => {
-    displayRoutes(app)
-    console.log(`Servidor activo en el puerto ${puerto}`);
-});
-
 // consume carpeta public
 app.use(express.json());
 app.use(express.urlencoded({ extends: true }));
@@ -27,3 +20,20 @@ app.use(express.static("./src/public"));
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 app.set("views", "./src/views");
+const io = new Server(http);
+
+app.listen(puerto, () => {
+    displayRoutes(app)
+    console.log(`Servidor activo en el puerto ${puerto}`);
+});
+
+
+io.on("connection", (socket) => {
+    console.log("Un cliente se conecto");
+
+    io.on("mensaje", (data) => {
+        mensajes.push(data);
+
+        io.emit("mensajesLogs", mensajes);
+    });
+});
