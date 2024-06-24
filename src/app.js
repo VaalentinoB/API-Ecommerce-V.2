e853d55a99c97a788b463ac040ef4c84f58311b4
VaalentinoB <valentinoburioni@hotmail.com>
2024-06-24 19:36:41 -0300

    views

--- src/app.js
+++ src/app.js
@@ -1,26 +1,34 @@
 import express from 'express';
 import cartsRouter from './routes/carts.routes.js';
-import { engine } from 'express-handlebars';
 import { Server } from 'socket.io';
+import { engine } from 'express-handlebars';
 import productsRouter from './routes/products.routes.js';
 import displayRoutes from 'express-routemap';
+import viewsRouter from "./routes/views.routes.js"
 
 const app = express();
 const puerto = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use('/api/products', productsRouter);
-app.use('/api/cart', cartsRouter);
-// consume carpeta public
-app.use(express.json());
-app.use(express.urlencoded({ extends: true }));
 app.use(express.static("./src/public"));
-// handlebars
-app.engine("handlebars", exphbs.engine());
+app.use("/", viewsRouter)
+
+
+app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./src/views");
-const io = new Server(http);
+
+
+
+
+app.use('/api/products', productsRouter);
+app.use('/api/cart', cartsRouter);
+
+
+
+
+
 
 app.listen(puerto, () => {
     displayRoutes(app)
@@ -28,12 +36,4 @@ app.listen(puerto, () => {
 });
 
 
-io.on("connection", (socket) => {
-    console.log("Un cliente se conecto");
 
-    io.on("mensaje", (data) => {
-        mensajes.push(data);
-
-        io.emit("mensajesLogs", mensajes);
-    });
-});
