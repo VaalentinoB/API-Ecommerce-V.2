bce9993806f07ede214b3bc6ca991c3459e1a306
VaalentinoB <valentinoburioni@hotmail.com>
2024-07-17 16:57:04 -0300

    controladores de mongo

---
+++ src/dao/db/cart-manager-db.js
@@ -0,0 +1,55 @@
+import CartModel from "./models/cart.model";
+
+
+
+class CartManager {
+
+    async crearCarrito() {
+        try {
+            const nuevoCarrito = new CartModel({ products: [] })
+            await nuevoCarrito.save()
+            return nuevoCarrito
+        } catch (error) {
+            console.log("Error al Crear carrito", error);
+        }
+    }
+
+    async getCarritoById(cartId) {
+        try {
+
+            const carrito = await CartModel.findById(cartId)
+
+            if (!carrito) {
+                throw new Error(`No existe un carrito con el id ${cartId}`);
+            } else {
+                return carrito;
+
+            }
+
+        } catch (error) {
+            console.error("Error al obtener el carrito por ID", error);
+            throw error;
+        }
+    }
+
+    async agregarProductoAlCarrito(cartId, productId, quantity = 1) {
+        try {
+            const carrito = await this.getCarritoById(cartId)
+
+            const existe = carrito.products.find(item => item.product.toString() === productId)
+            if (existe) {
+                existe.quantity += quantity
+            } else {
+                carrito.products.push({ product: productId, quantity })
+            }
+            carrito.markModified("products")
+            await carrito.save()
+            return carrito
+
+        } catch (error) {
+            console.log("Error al agrefar producto", error);
+        }
+    }
+}
+
+export default CartManager;
---
+++ src/dao/db/product-manager-db.js
@@ -0,0 +1,104 @@
+import ProductModel from "./models/product.model";
+
+class ProductManager {
+
+
+    async addProduct({ title, description, code, stock, category, image, price, thumbnails }) {
+        try {
+
+            if (!title || !description || !price || !code || !stock || !category) {
+                console.log("¡¡¡Todos los campos son obligatorios!!!" + " Validacion numero 2 XD");
+                return;
+            }
+
+            const existeProducto = await ProductModel.findOne({ code: code })
+
+            if (existeProducto) {
+                console.log("EL PRODUCTO YA EXISTE!")
+                return;
+            }
+
+            const newProduct = new ProductModel({
+                title,
+                description,
+                code,
+                status: true,
+                stock,
+                category,
+                image,
+                price,
+                thumbnails: thumbnails || []
+            });
+
+            await newProduct.save()
+
+
+        } catch (error) {
+            console.log("Error al agregar producto", error);
+            throw error;
+        }
+    }
+    async getProducts() {
+        try {
+            const arrayProductos = await ProductModel.find()
+            return arrayProductos
+        } catch (error) {
+            console.log("Error al leer el archivo", error);
+            throw error;
+        }
+    }
+
+    async getProductById(id) {
+        try {
+            const buscado = await ProductModel.findById(id)
+
+            if (!buscado) {
+                console.log("Producto no encontrado");
+                return null;
+            } else {
+                console.log("Producto encontrado");
+                return buscado;
+            }
+        } catch (error) {
+            console.log("Error al leer el archivo", error);
+            throw error;
+        }
+    }
+
+    async updateProduct(id, productoActualizado) {
+        try {
+
+            const producto = await ProductModel.findByIdAndUpdate(id, productoActualizado)
+
+            if (!producto) {
+                console.log("No encontre el producto :/");
+            } else {
+                console.log("PRODUCTO ACTUALIZADO!!");
+                return producto;
+            }
+        } catch (error) {
+            console.log("Error al actualizar el producto", error);
+            throw error;
+        }
+    }
+
+    async deleteProduct(id) {
+        try {
+
+            const borrado = await ProductModel.findByIdAndDelete(id);
+            if (!borrado) {
+                console.log("No lo encontre :/");
+                return null;
+            } else {
+                console.log("Producto eliminado")
+                return borrado;;
+            }
+
+        } catch (error) {
+            console.log("Error al eliminar el producto", error);
+            throw error;
+        }
+    }
+}
+
+export default ProductManager;
