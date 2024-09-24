be34cfb8684de55564ecb393df4f72ec355026b0
VaalentinoB <valentinoburioni@hotmail.com>
2024-09-24 19:56:01 -0300

    ticket

--- src/models/ticket.model.js
+++ src/models/ticket.model.js
@@ -2,8 +2,22 @@ import mongoose from "mongoose";
 
 
 const ticketSchema = new mongoose.Schema({
-    code,
-    purchase_datetime,
-    amount,
+    code:{
+        type: String,
+        required: true,
+        unique: true,
+        default: function() {
+            return `TICKET- ${Math.random().toString()}`
+        }
+    },
+    purchase_datetime: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    amount {
+        type: Number,
+        required: true
+    },
     purchaser
 })
